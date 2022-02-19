093628679d7e0b66a0df54f0634b99e88132190a
Devon Langendoerfer <devon.langendoerfer@konghq.com>
2022-02-18 23:47:40 -0800

    update my awesome logo

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="grr logo" src="./assets/grr.png">
     <HelloWorld msg="Action Flow Testing"/>
   </div>
 </template>
