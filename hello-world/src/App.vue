8cd457765f2fb46c0940cb778b2b9b7d651ba253
Devon Langendoerfer <devon.langendoerfer@konghq.com>
2022-02-21 02:48:03 -0800

    derrrrrp

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="grr logo" src="./assets/grr.png">
-    <HelloWorld msg="Action Flow Testing v0.0.1-6"/>
+    <HelloWorld msg="Action Flow Testing v0.0.1-7"/>
   </div>
 </template>
 
