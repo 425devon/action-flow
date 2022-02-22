fa28d75dd08721a64e79c72d46178e004821cc03
Devon Langendoerfer <devon.langendoerfer@konghq.com>
2022-02-22 13:53:30 -0800

    fix output tags

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="grr logo" src="./assets/grr.png">
-    <HelloWorld msg="Action Flow Testing v0.0.2-7"/>
+    <HelloWorld msg="Action Flow Testing v0.0.2-8"/>
   </div>
 </template>
 
